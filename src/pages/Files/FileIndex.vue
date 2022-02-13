0f3cd6c7f52b355bd85e0eb4c158ff65aad19f44
Michał Gurgul <mgurgul@telecube.pl>
2022-02-13 16:40:42 +0100

    Files  update

--- src/pages/Files/FileIndex.vue
+++ src/pages/Files/FileIndex.vue
@@ -2,8 +2,19 @@
   <div class="row justify-center text-blue-grey-10">
     <q-page class="col-lg-8 col-sm-10 col-xs q-pa-xs">
       <div class="q-pa-md q-gutter-sm">
-        <div v-for="task in urls" v-bind:key="task.id">
-          <q-img :src="task.img" spinner-color="white" style="height: 140px; max-width: 150px" />
+        <div width="100%" v-for="img in imgs" v-bind:key="img.id">
+          <q-img :src="img.img" spinner-color="black" style="height: 140px; max-width: 150px">
+            <q-icon
+              class="absolute all-pointer-events"
+              size="sm"
+              name="delete"
+              color="white"
+              style="top: 8px; right: 8px"
+              @click="deleteFile(img.id)"
+            >
+              <q-tooltip>Tooltip</q-tooltip>
+            </q-icon>
+          </q-img>
         </div>
       </div>
     </q-page>
@@ -18,18 +29,23 @@ import { ref, reactive } from 'vue'
 export default defineComponent({
   name: "PageIndex",
   setup() {
-    const urls = ref('[{"id" :1 , "img": "https://placeimg.com/500/300/nature?t=1"}, {"id" :2 , "img": "https://placeimg.com/500/300/nature?t=2"}]')
-    function randImg(no) {
-      return String('https://placeimg.com/500/300/nature?t=' + no);
+    const imgs = ref([
+      { "id": 1, "img": "https://placeimg.com/500/300/nature?t=1" },
+      { "id": 2, "img": "https://placeimg.com/500/300/nature?t=2" },
+      { "id": 3, "img": "https://placeimg.com/500/300/nature?t=3" },
+      { "id": 4, "img": "https://placeimg.com/500/300/nature?t=4" },
+    ])
+
+
+    function deleteFile(id) {
+      alert(id);
     }
 
 
+
     return {
-      urls,
-      randImg,
-      refresh() {
-        url.value = 'https://placeimg.com/500/300/nature?t=' + Math.random()
-      }
+      imgs,
+      deleteFile
     };
   },
 });
