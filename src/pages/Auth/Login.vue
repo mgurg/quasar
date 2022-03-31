b7c08fa6ba0d041560888b5e635a854ff1c9a795
Michał Gurgul <mgurgul@telecube.pl>
2022-03-31 18:40:55 +0200

    Login process

--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -1,86 +1,85 @@
 <template>
-    <!-- https://github.com/Joabsonlg/quasar-authentication -->
-    <!-- https://github.com/patrickmonteiro/quasar-warren -->
-    <div class="row justify-center" style="height: 100vh">
-        <div class="col-xs-12 col-sm-6 flex container-logo bg-blue-grey-7" v-if="fade">
-            <div class="column self-center q-mx-auto">
-                <!-- <img
+  <!-- https://github.com/Joabsonlg/quasar-authentication -->
+  <!-- https://github.com/patrickmonteiro/quasar-warren -->
+  <div class="row justify-center" style="height: 100vh">
+    <div class="col-xs-12 col-sm-6 flex container-logo bg-blue-grey-7" v-if="fade">
+      <div class="column self-center q-mx-auto">
+        <!-- <img
                     class="q-mx-auto"
                     src="~assets/quasar-logo-vertical.svg"
                     :width="$q.screen.gt.sm ? 'auto' : '50'"
                 />-->
-                <!-- <div
+        <!-- <div
                     :class="
                         $q.screen.gt.sm
                             ? 'text-center text-h2 text-weight-bolder text-white'
                             : 'text-center text-h5 text-weight-bolder text-white'
                     "
                 >-->
-                <!-- Intio -->
-                <div class="q-ma-lg text-left text-white power-text">
-                    <h1 class="text-h3">Intio</h1>
-                    <p class="text-h4 text-weight-light">Empowering life safety with technology</p>
-                    <p
-                        class="text-h6"
-                    >With over 40-years of experience in life-safety systems, we are revolutionizing industries that protect us most.</p>
-                </div>
-                <!-- </div> -->
-            </div>
+        <!-- Intio -->
+        <div class="q-ma-lg text-left text-white power-text">
+          <h1 class="text-h3">Intio</h1>
+          <p class="text-h4 text-weight-light">Empowering life safety with technology</p>
+          <p class="text-h6">
+            With over 40-years of experience in life-safety systems, we are
+            revolutionizing industries that protect us most.
+          </p>
         </div>
-        <!--  -->
-        <div class="column q-gutter-y-lg q-pa-md self-center q-mx-auto">
-            <div
-                :class="
-                    $q.screen.gt.sm
-                        ? 'text-h5 text-weight-bold'
-                        : 'text-h6 text-weight-bold'
-                "
-            >Zaloguj się</div>
+        <!-- </div> -->
+      </div>
+    </div>
+    <!--  -->
+    <div class="column q-gutter-y-lg q-pa-md self-center q-mx-auto">
+      <div
+        :class="$q.screen.gt.sm ? 'text-h5 text-weight-bold' : 'text-h6 text-weight-bold'"
+      >
+        {{ $t("Login") }}
+      </div>
 
-            <q-form @submit="submit">
-                <q-input
-                    v-model="email"
-                    :disable="isLoading"
-                    :error="!!errors.email"
-                    :error-message="errors.email"
-                    class="q-mb-md"
-                    :label="$t('Email')"
-                    outlined
-                    type="email"
-                />
-                <q-input
-                    v-model="password"
-                    :disable="isLoading"
-                    :error="!!errors.password"
-                    :error-message="errors.password"
-                    :type="isPwd ? 'password' : 'text'"
-                    :label="$t('Password')"
-                    outlined
-                >
-                    <template v-slot:append>
-                        <q-icon
-                            :name="isPwd ? 'visibility_off' : 'visibility'"
-                            class="cursor-pointer"
-                            @click="isPwd = !isPwd"
-                        />
-                    </template>
-                </q-input>
+      <q-form @submit="submit">
+        <q-input
+          v-model="email"
+          :disable="isLoading"
+          :error="!!errors.email"
+          :error-message="errors.email"
+          class="q-mb-md"
+          :label="$t('E-mail')"
+          outlined
+          type="email"
+        />
+        <q-input
+          v-model="password"
+          :disable="isLoading"
+          :error="!!errors.password"
+          :error-message="errors.password"
+          :type="isPwd ? 'password' : 'text'"
+          :label="$t('Password')"
+          outlined
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
 
-                <q-checkbox v-model="rememberUser" :label="$t('Remember me for 30 days')" />
-                {{ errorMsg }}
-                <div class="row">
-                    <q-space />
-                    <q-btn
-                        :disable="isLoading"
-                        :label="$t('Login')"
-                        :loading="isLoading"
-                        color="primary"
-                        type="submit"
-                    />
-                </div>
-            </q-form>
+        <q-checkbox v-model="rememberUser" :label="$t('Remember me for 30 days')" />
+        {{ errorMsg }}
+        <div class="row">
+          <q-space />
+          <q-btn
+            :disable="isLoading"
+            :label="$t('Login')"
+            :loading="isLoading"
+            color="primary"
+            type="submit"
+          />
+        </div>
+      </q-form>
 
-            <!-- <label v-if="!login">E-mail</label>
+      <!-- <label v-if="!login">E-mail</label>
             <q-input required v-if="!login" filled bottom-slots v-model="email">
                 <template v-slot:prepend>
                     <q-icon name="email" />
@@ -105,19 +104,18 @@
                     <q-spinner-gears />
                 </template>
             </q-btn>-->
-            <div class="divider q-mt-lg q-mb-sm"></div>
-            <div class="row">
-                <div class="col-12">
-                    <p class="text-body2 q-pa-xs">
-                        <a href="#">{{ $t('Register') }}</a> ·
-                        <a href="#">{{ $t('Register') }}</a> ·
-                        <router-link to="reset_password">{{ $t('Reset password') }}</router-link>
-                        · {{ $t('I need help') }}
-                    </p>
-                </div>
-            </div>
+      <div class="divider q-mt-lg q-mb-sm"></div>
+      <div class="row">
+        <div class="col-12">
+          <p class="text-body2 q-pa-xs">
+            <a href="#">{{ $t("Register") }}</a> · <a href="#">{{ $t("Register") }}</a> ·
+            <router-link to="reset_password">{{ $t("Reset password") }}</router-link>
+            · {{ $t("I need help") }}
+          </p>
+        </div>
+      </div>
 
-            <!-- <div class="row">
+      <!-- <div class="row">
                 <div class="col-12">
                     <div class="row justify-center q-gutter-lg">
                         <div class="col-5">
@@ -153,16 +151,15 @@
                     </div>
                 </div>
             </div>-->
-        </div>
     </div>
+  </div>
 </template>
 <script>
 import { useRouter } from "vue-router";
-// import { useStore } from "vuex";
+import { useUserStore } from 'stores/user'
 import { ref } from "vue";
 import { useField, useForm } from "vee-validate";
-import { object, string, bool } from 'yup'
-
+import { object, string, bool } from "yup";
 
 let isLoading = ref(false);
 let isError = ref(false);
@@ -172,80 +169,79 @@ let login = ref(false);
 let fade = ref(true);
 
 export default {
-    props: { tab: { type: String } },
-    setup() {
-        const router = useRouter();
+  props: { tab: { type: String } },
+  setup() {
+    const router = useRouter();
+    const UserStore = useUserStore()
 
-        // const authStore = useStore();
+    // const authStore = useStore();
 
-        // -------------- VeeValidate --------------
+    // -------------- VeeValidate --------------
 
-        const validationSchema = object({
-            email: string().required('Provide an valid email').email(),
-            password: string().required(),
-            rememberUser: bool().required()
+    const validationSchema = object({
+      email: string().required("Provide an valid email").email(),
+      password: string().required(),
+      rememberUser: bool().required(),
+    });
 
-        })
+    const { handleSubmit, errors } = useForm({
+      validationSchema,
+    });
 
-        const { handleSubmit, errors } = useForm({
-            validationSchema
-        })
+    const { value: email, handleChange } = useField("email");
+    const { value: password } = useField("password");
+    const { value: rememberUser } = useField("rememberUser", undefined, {initialValue: false});
 
-        const { value: email, handleChange } = useField('email')
-        const { value: password } = useField('password')
-        const { value: rememberUser } = useField('rememberUser', undefined, { initialValue: false })
+    const submit = handleSubmit((values) => {
+      console.log("submit", values);
 
+      let data = {
+        email: email.value,
+        password: password.value,
+        permanent: rememberUser.value,
+      };
+      LoginUser(data);
+    });
 
-        const submit = handleSubmit(values => {
-            console.log('submit', values);
+    // --------------- VeeValidate --------------
 
-            let data = {
-                "email": email.value,
-                "password": password.value,
-                "permanent": rememberUser.value
-            }
-            LoginUser(data);
-        })
-
-        // --------------- VeeValidate --------------
-
-        async function LoginUser(data) {
-            isLoading.value = true;
-            // try {
-            //     await authStore.dispatch("authModule/loginUser", data).then(() => {
-            //         console.log('OK')
-            //         router.replace("/");
-            //     }).catch((error) => {
-            //         errorMsg.value = error
-            //     });
-
-            // } catch (err) {
-            //     console.log("Err: " + err);
-            // }
-
-            isLoading.value = false;
+    async function LoginUser(data ) {
+      isLoading.value = true;
+        try{
+            await UserStore.fetchUsers(data.email, data.password, data.permanent)
+            router.push({path:"/"})
+        }catch(err){
+          console.log(err)
+            // console.log(err.error_description || err.message)
+            // console.log(err.data)
+            // errorMsg.value = err
         }
 
-        return {
-            loading1,
-            login,
-            fade,
-            email,
-            password,
-            rememberUser,
-            errors,
-            isLoading,
-            isError,
-            errorMsg,
-            isPwd: ref(true),
-            submit
-        };
-    },
+
+
+      isLoading.value = false;
+    }
+
+    return {
+      loading1,
+      login,
+      fade,
+      email,
+      password,
+      rememberUser,
+      errors,
+      isLoading,
+      isError,
+      errorMsg,
+      isPwd: ref(true),
+      submit,
+    };
+  },
 };
 </script>
 
 <style>
 .divider {
-    border-top: 1px solid #ebecf3;
+  border-top: 1px solid #ebecf3;
 }
 </style>
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,8 +1,9 @@
 import { defineStore } from 'pinia';
+import { api } from "boot/axios";
 
 export const useUserStore = defineStore('user', {
   state: () => ({
-    token: 123,
+    token: null,
     permissions: [],
     firstName: null,
     lastName: null,
@@ -17,5 +18,42 @@ export const useUserStore = defineStore('user', {
     increment() {
       this.counter++;
     },
+    async fetchUsers(email,password,permanent) {
+      try {
+        const body = {
+          "email": email,
+          "password": password,
+          "permanent": permanent
+        }
+        console.log(body)
+        const data = await api.post('/auth/login',body)
+        console.log(data.data)
+          this.token = data.data.auth_token
+          this.firstName = data.data.auth_token
+          this.lastName = data.data.auth_token
+          this.tz = data.data.auth_token
+          this.lang = data.data.auth_token
+
+          if (permanent == true) {
+            localStorage.setItem("klucz", data.data.auth_token);
+          } else {
+            sessionStorage.setItem("klucz", data.data.auth_token);
+          }
+          return('OK')
+        }
+        catch (error) {
+          // alert(error)
+          // console.log(error.data)
+          // throw error;
+          if (error.response) {
+            console.log(error.response.data.detail);
+          } else if (error.request) {
+            console.log(error.request);
+          } else {
+            console.log("General Error");
+          }
+          throw error.response.data.detail;
+      }
+    }
   },
 });
