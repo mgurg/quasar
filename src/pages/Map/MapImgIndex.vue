2e2d9bacfb61d52bc2654de654048749139508f8
Michal Gurgul <m.gurgul@telecube.pl>
2022-04-08 09:32:36 +0200

    Image overlay

--- src/pages/Map/MapImgIndex.vue
+++ src/pages/Map/MapImgIndex.vue
@@ -1,96 +1,40 @@
 <template>
   <q-layout>
     <q-page class="flex flex-center">
-      <div>
-        <!-- <q-btn @click="addMarker">Add Circle</q-btn>
-        <q-btn @click="addPin">Add Pin</q-btn> -->
-        <p>Center is at {{ currentCenter.lat }}, {{ currentCenter.lng }} and the zoom is: {{ currentZoom }}</p>
-      </div>
-
-      <l-map style="height:95vh" ref="map" v-model:zoom="zoom">
-      <l-image-overlay url="https://vscda.org/wp-content/uploads/2017/03/300x300.png" :bounds="bounds" ></l-image-overlay></l-map>
-         
-
+      <l-map style="height: 350px" :crs="crs" :options="{ zoomControl: false }">
+        <!--  -->
+        <l-image-overlay :url="url" :bounds="bounds"></l-image-overlay>
+        <l-marker v-for="star in stars" :lat-lng="star" :key="star.name">
+          <l-popup :content="star.name" />
+        </l-marker>
+      </l-map>
     </q-page>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref, reactive } from 'vue';
-import { latLng, imageOverlay } from "leaflet"
-import "leaflet/dist/leaflet.css"
-import { LMap, LGeoJson, LMarker,LTileLayer, LCircle, LPopup, LCircleMarker , LImageOverlay} from "@vue-leaflet/vue-leaflet";
+import { LMap, LImageOverlay, LMarker, LPopup } from '@vue-leaflet/vue-leaflet';
+import { latLng, CRS } from "leaflet"
 
-
-export default defineComponent({
-  name: 'PageIndex',
+export default {
   components: {
     LMap,
-    LGeoJson,
-    LTileLayer,
-    LCircle,
-    LPopup,
+    LImageOverlay,
     LMarker,
-    LCircleMarker
+    LPopup
   },
-
-  setup() {
-
-
-
-const defaultLocation = latLng(49.0139, 31.2858)
-  const currentZoom = ref(11.5)
-  const currentCenter = ref(latLng(47.41322, -1.219482))
-
-  // https://github.com/timyboy12345/Themepark-Escape/blob/a1f10bf18d4c41b59286587091d54e2b376d551e/src/components/MapComponent.vue
-
-// var w = 300,
-//     h = 300,
-//     url = 'http://kempe.net/images/newspaper-big.jpg';
-
-// // calculate the edges of the image, in coordinate space
-// var southWest = LMap.unproject([0, h], 3);
-// var northEast = LMap.unproject([w, 0], 3);
-// var bounds = new L.LatLngBounds(southWest, northEast);
-
-// https://kempe.net/blog/2014/06/14/leaflet-pan-zoom-image.html
-
-    function onMapClick(value) {
-      // place the marker on the clicked spot
-      alert(value.latlng);
-    }
-
-    const zoomUpdate = (zoom) => {
-          currentZoom.value = zoom;
-      }
-    
-    const centerUpdate = (center) => {
-        currentCenter.value = center;
-      }
-
-
-    // https://medium.com/swlh/create-an-interactive-location-selector-with-vue-js-and-leaflet-5808c55b4636
-    // https://vue2-leaflet.netlify.app/examples/simple.html
-
-    // https://github.com/dev-eugen/vpe/blob/master/src/containers/Dashboard/views/ProductsMap.vue
-
+  data() {
     return {
-      zoom: 6,
-      mozCenter: [-19.4514005, 34.05761718],
-      userLocation : [-19.4514005, 34.05761718],
-      defaultLocation,
-
-      currentZoom,
-      currentCenter,
-      onMapClick,
-      centerUpdate,
-      zoomUpdate,
-      bounds: [
-        [0, 0],
-        [1000, 1000],
-      ],
+      url: 'https://www.veryfrenchgangsters.com/include/asset/leaflet/docs/examples/crs-simple/uqm_map_full.png',
+      bounds: [[-26.5, -25], [1021.5, 1023]],
+      crs: CRS.Simple,
+      stars: [
+        { name: 'Sol', lng: 175.2, lat: 145.0 },
+        { name: 'Mizar', lng: 41.6, lat: 130.1 },
+        { name: 'Krueger-Z', lng: 13.4, lat: 56.5 },
+        { name: 'Deneb', lng: 218.7, lat: 8.3 }
+      ]
     };
   }
-
-})
+}
 </script>
