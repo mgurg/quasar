48e660d1f1fc415ab5444e42d0f2012e46378e48
Michal Gurgul <m.gurgul@telecube.pl>
2022-09-05 16:14:43 +0200

    feat(PWA): StaleWhileRevalidate Strategy for http requests

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -1,11 +1,53 @@
 /*
  * This file (which will be your service worker)
  * is picked up by the build system ONLY if
- * quasar.config.js > pwa > workboxPluginMode is set to "InjectManifest"
+ * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
 
-import { precacheAndRoute } from 'workbox-precaching'
+/*
+  dependencies
+*/
 
-console.log('Custom Service Worker')
-// Use with precache injection
-precacheAndRoute(self.__WB_MANIFEST)
+import {precacheAndRoute} from 'workbox-precaching'
+import {registerRoute} from 'workbox-routing'
+import {StaleWhileRevalidate} from 'workbox-strategies'
+import {CacheFirst} from 'workbox-strategies'
+import {ExpirationPlugin} from 'workbox-expiration'
+import {CacheableResponsePlugin} from 'workbox-cacheable-response'
+import {NetworkFirst} from 'workbox-strategies';
+
+
+/*
+config
+*/
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+/*
+caching strategies
+*/
+
+// registerRoute(
+//   ({url}) => url.host.startsWith('fonts.g'),
+//   new CacheFirst({
+//     cacheName: 'google-fonts',
+//     plugins: [
+//       new ExpirationPlugin({
+//         maxEntries: 30,
+//       }),
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200]
+//       }),
+//     ],
+//   })
+// );
+
+// registerRoute(
+//   ({url}) => url.pathname.startsWith('/posts'),
+//   new NetworkFirst()
+// );
+
+registerRoute(
+  ({url}) => url.href.startsWith('http'),
+  new StaleWhileRevalidate()
+);
