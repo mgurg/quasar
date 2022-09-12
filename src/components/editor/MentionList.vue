e013e6f5ed94f83d82c7d70eadf1f78e4cec85b9
mgur <mgur@pop-os.localdomain>
2022-09-12 18:49:02 +0200

    feat: UUID support in mentions

--- src/components/editor/MentionList.vue
+++ src/components/editor/MentionList.vue
@@ -2,91 +2,94 @@
     <div class="items">
       <template v-if="items.length">
         <button
-          class="item"
-          :class="{ 'is-selected': index === selectedIndex }"
-          v-for="(item, index) in items"
-          :key="index"
-          @click="selectItem(index)"
-        >
-          {{ item }}
+      class="item"
+      :class="{ 'is-selected': index === selectedIndex }"
+      v-for="(item, index) in items"
+      :key="index"
+      @click="selectItem(index)"
+    >
+          {{ item.label }}
         </button>
       </template>
       <div class="item" v-else>
         No result
-      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      items: {
-        type: Array,
-        required: true,
-      },
-  
-      command: {
-        type: Function,
-        required: true,
-      },
+  </div>
+</template>
+
+<!-- https://github.com/15653391491/monaco_web/blob/97a173b89c44f13b5be01a219630a11d3b1b4f45/src/components/common/MentionList.vue -->
+
+<script>
+export default {
+  props: {
+    items: {
+      default: () => [],
+      type: Array,
+      required: true,
     },
   
-    data() {
-      return {
-        selectedIndex: 0,
+    command: {
+      type: Function,
+      required: true,
+    },
+  },
+  
+  data() {
+    return {
+      selectedIndex: 0,
+    }
+  },
+  
+  watch: {
+    items() {
+      this.selectedIndex = 0
+    },
+  },
+  
+  methods: {
+    onKeyDown({ event }) {
+      if (event.key === 'ArrowUp') {
+        this.upHandler()
+        return true
+      }
+  
+      if (event.key === 'ArrowDown') {
+        this.downHandler()
+        return true
+      }
+  
+      if (event.key === 'Enter') {
+        this.enterHandler()
+        return true
+      }
+  
+      return false
+    },
+  
+    upHandler() {
+        this.selectedIndex = ((this.selectedIndex + this.items.length) - 1) % this.items.length
+    },
+  
+    downHandler() {
+        this.selectedIndex = (this.selectedIndex + 1) % this.items.length
+    },
+  
+    enterHandler() {
+      this.selectItem(this.selectedIndex)
+    },
+  
+    selectItem(index) {
+      const item = this.items[index]
+  
+      if (item) {
+        this.command(item)
       }
     },
+  },
+}
+</script>
   
-    watch: {
-      items() {
-        this.selectedIndex = 0
-      },
-    },
-  
-    methods: {
-      onKeyDown({ event }) {
-        if (event.key === 'ArrowUp') {
-          this.upHandler()
-          return true
-        }
-  
-        if (event.key === 'ArrowDown') {
-          this.downHandler()
-          return true
-        }
-  
-        if (event.key === 'Enter') {
-          this.enterHandler()
-          return true
-        }
-  
-        return false
-      },
-  
-      upHandler() {
-        this.selectedIndex = ((this.selectedIndex + this.items.length) - 1) % this.items.length
-      },
-  
-      downHandler() {
-        this.selectedIndex = (this.selectedIndex + 1) % this.items.length
-      },
-  
-      enterHandler() {
-        this.selectItem(this.selectedIndex)
-      },
-  
-      selectItem(index) {
-        const item = this.items[index]
-  
-        if (item) {
-          this.command({ id: item })
-        }
-      },
-    },
-  }
-  </script>
-  
-  <style lang="scss">
+<style lang="scss">
   .items {
     padding: 0.2rem;
     position: relative;
@@ -116,4 +119,3 @@
     }
   }
   </style>
-  
