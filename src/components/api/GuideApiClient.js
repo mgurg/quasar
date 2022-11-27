a7354e848016f43c931276f34186b4aeb52f3b00
mgur <mgur@pop-os.localdomain>
2022-11-27 16:19:48 +0100

    Axios requests and error handling extracted to separate files

--- src/components/api/GuideApiClient.js
+++ src/components/api/GuideApiClient.js
@@ -1,18 +1,16 @@
 // https://daily.dev/blog/a-guide-to-writing-clean-api-calls-using-axios
 // https://blog.openreplay.com/making-api-calls-in-vue-with-axios/
 
-
 import { authApi } from "boot/axios";
 
-
-export function getGuide(){
-    return authApi.get('/guides/');
+export function getGuide() {
+  return authApi.get("/guides/");
 }
 
-export function addGuide(data){
-    return authApi.post('/guides/1', data);
+export function addGuide(data) {
+  return authApi.post("/guides/", data);
 }
 
-export function editGuide(uuid, data){
-    return authApi.post('/guides/'+uuid, JSON.stringify(data));
+export function editGuide(uuid, data) {
+  return authApi.post("/guides/" + uuid, data);
 }
