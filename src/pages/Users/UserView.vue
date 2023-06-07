386a613576577338213e2b83e2e665ce02380755
mgur <mgur@pop-os.localdomain>
2023-06-07 21:32:29 +0200

    fix remove unsued getUserIdeas

--- src/pages/Users/UserView.vue
+++ src/pages/Users/UserView.vue
@@ -341,21 +341,21 @@ function activateUser(uuid) {
   });
 }
 
-function getUserIdeas(uuid) {
-  // userUuid.value
-  isLoading.value = true;
-  getUserIdeasRequest(uuid).then(function (response) {
-    ideas.value = response.data.items
-
-    if (response.data.items.length === 0) {
-      expandedIdeas.value = false;
-    }
-    isLoading.value = false;
-  }).catch((err) => {
-    const errorMessage = errorHandler(err);
-    isError.value = true;
-  });
-}
+// function getUserIdeas(uuid) {
+//   // userUuid.value
+//   isLoading.value = true;
+//   getUserIdeasRequest(uuid).then(function (response) {
+//     ideas.value = response.data.items
+//
+//     if (response.data.items.length === 0) {
+//       expandedIdeas.value = false;
+//     }
+//     isLoading.value = false;
+//   }).catch((err) => {
+//     const errorMessage = errorHandler(err);
+//     isError.value = true;
+//   });
+// }
 
 
 function getUserIssues(uuid) {
@@ -401,7 +401,7 @@ function deleteUser(uuid) {
 onBeforeMount(() => {
   isLoading.value = true;
   getUserDetails(route.params.uuid);
-  getUserIdeas(route.params.uuid);
+  // getUserIdeas(route.params.uuid);
   getUserIssues(route.params.uuid);
 });
 
