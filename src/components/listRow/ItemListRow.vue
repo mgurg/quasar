2ad060dfe563daeb3e00ecc8450c68fa18254d68
mgurgul <mgurgul@pop-os.localdomain>
2023-07-28 11:37:08 +0200

    description

--- src/components/listRow/ItemListRow.vue
+++ src/components/listRow/ItemListRow.vue
@@ -16,8 +16,8 @@
 
     <q-item-section side v-if="props.displayMode === null">
       <div class="text-grey-8 q-gutter-xs">
-        <q-btn size="12px" flat dense icon="post_add" color="blue-12" @click="addGuide(item.uuid)"></q-btn>
-        <q-btn size="12px" flat dense icon="bug_report" color="red-12" @click="reportFailure(item.uuid)"></q-btn>
+        <q-btn dense no-caps flat  icon="post_add" color="blue-12" @click="addGuide(item.uuid)" :label="$q.screen.gt.xs ? 'Nowy poradnik' : ''"></q-btn>
+        <q-btn dense no-caps flat  icon="bug_report" color="red-12" @click="reportFailure(item.uuid)" :label="$q.screen.gt.xs ? 'Awaria!' : ''"></q-btn>
 
       </div>
         <!-- <q-item-label caption><q-icon name="star" color="warning" size="2rem" ></q-icon></q-item-label> -->
