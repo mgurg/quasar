e51ce5813d3a05dd9ec0cd6602b6a57f037bcf5f
mgurg <34517378+mgurg@users.noreply.github.com>
2024-05-16 11:52:33 +0200

    WIP: api composable

---
+++ src/composables/useNoAuthAPI.js
@@ -0,0 +1,49 @@
+import {noAuthApi} from "boot/axios";
+
+const request = {
+  async safe(funcCall, url, data) {
+    let error = null;
+    const response = await funcCall(url, data).catch(function (e) {
+      // console.log(e); # TODO add simple error message logging?
+      // Insert Generic Error Handling Here
+      error = e;
+      return null;
+    });
+    return {response, error, data: response?.data ?? null};
+  },
+};
+
+function getRequests(axiosInstance) {
+  return {
+    async get(url, params = {}) {
+      let error = null;
+      const response = await axiosInstance.get(url, params).catch((e) => {
+        error = e;
+      });
+      if (response != null) {
+        return {response, error, data: response?.data};
+      }
+      return {response: null, error, data: null};
+    },
+
+    async post(url, data) {
+      return await request.safe(axiosInstance.post, url, data);
+    },
+
+    async put(url, data) {
+      return await request.safe(axiosInstance.put, url, data);
+    },
+
+    async patch(url, data) {
+      return await request.safe(axiosInstance.patch, url, data);
+    },
+
+    async delete(url) {
+      return await request.safe(axiosInstance.delete, url, undefined);
+    },
+  };
+}
+
+export const useNoAuthAPI = function () {
+  return getRequests(noAuthApi);
+};
