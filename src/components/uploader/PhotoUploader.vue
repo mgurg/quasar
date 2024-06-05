e0f548bef72067611d79a4ad76796fe2a0ee483a
mgurg <34517378+mgurg@users.noreply.github.com>
2024-06-05 15:24:10 +0200

    WIP: Photo upload

--- src/components/uploader/PhotoUploader.vue
+++ src/components/uploader/PhotoUploader.vue
@@ -16,9 +16,18 @@
     </template>
     <template v-slot:file="{ file }">
       <q-chip
-        :removable="isUploading" class="full-width q-my-xs" square @remove="cancelFile()">
-        <q-linear-progress :value="uploadProgress" class="absolute-full full-height" color="green-2" stripe
-                           track-color="grey-2"/>
+        :removable="isUploading"
+        class="full-width q-my-xs"
+        square
+        @remove="cancelFile()"
+      >
+        <q-linear-progress
+          :value="uploadProgress"
+          class="absolute-full full-height"
+          color="green-2"
+          stripe
+          track-color="grey-2"
+        />
 
         <q-avatar>
           <q-icon name="photo"/>
@@ -35,17 +44,26 @@
     </template>
   </q-file>
 
-  <!-- IMG -->
   <div class="row q-col-gutter-xs">
-    <div v-for="(file, index) in attachments" v-bind:key="index" class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+    <div
+      v-for="(file, index) in attachments"
+      :key="index"
+      class="col-xs-6 col-sm-6 col-md-3 col-lg-3"
+    >
       <q-img
         :src="file.url"
         fit="cover"
         spinner-color="black"
-        style="height: 300px; width: 100%; border: 1px solid gray;"
+        class="image-preview"
       >
-        <q-icon class="absolute all-pointer-events" color="blue-grey-5" name="delete" size="sm"
-                style="top: 8px; right: 8px" @click="delete_file(file.uuid)">
+        <q-icon
+          class="absolute all-pointer-events"
+          color="blue-grey-5"
+          name="delete"
+          size="sm"
+          style="top: 8px; right: 8px"
+          @click="deleteFile(file.uuid)"
+        >
           <q-tooltip>Remove image</q-tooltip>
         </q-icon>
         <template v-slot:error>
@@ -60,7 +78,7 @@
 </template>
 
 <script setup>
-import {reactive, ref} from "vue";
+import {ref} from "vue";
 import {api} from "boot/axios";
 import {useUserStore} from "stores/user";
 import Compressor from "compressorjs";
@@ -68,65 +86,41 @@ import {useQuasar} from "quasar";
 import {useI18n} from "vue-i18n";
 import {useRoute, useRouter} from "vue-router";
 import {useNoAuthAPI} from "src/composables/useNoAuthAPI.js";
-// import {errorHandler} from "components/api/errorHandler";
-// import {deleteFileRequest} from "components/api/FilesApiClient";
-
-// https://github.com/sjq4499/vite-vue3/blob/8ffaf0cda0cf6d15e30242d97d6d2eaa824f1eb6/src/views/tool/compressImages.vue
-// https://github.com/H37kouya/miya-meshi/blob/736598180c428465628c14ca165831c04961d12f/admin/components/organisms/file/UploadImageFile.vue
+import {deleteFileRequest} from "components/api/FilesApiClient";
 
 const props = defineProps({
   fileList: {
-    type: Object,
-    default() {
-      return {
-        uuid: null,
-        file_name: null,
-        extension: null,
-        mimetype: null,
-        size: null,
-        url: null,
-      };
-    },
+    type: Array,
+    default: () => [],
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+  tenant_id: {
+    type: String,
+    default: null,
   },
 });
 
 const emit = defineEmits(["uploadedPhotos"]);
 
-const $q = useQuasar()
+const $q = useQuasar();
 const {t} = useI18n();
-const route = useRoute();
-const router = useRouter();
 const UserStore = useUserStore();
 const noAuthAPI = useNoAuthAPI();
 
-// const $q = useQuasar()
-// const {t} = useI18n({ useScope: "global" });
-// const confirmDeleteMessage = computed(() => t("Delete:"));
-// const successfulDeleteMessage = computed(() => t("Deleted:"));
-
-let attachments = ref([]);
-let newAttachments = ref([]);
-if (props.fileList !== null) {
-  attachments.value = props.fileList;
-  attachments.value = attachments.value.filter((item) => item.mimetype.match('image.*'))
-  emit('uploadedPhotos', attachments.value)
-}
-
-
-
+const attachments = ref(props.fileList ? props.fileList.filter((item) => item.mimetype.match("image.*")) : []);
+emit("uploadedPhotos", attachments.value);
 
 const files = ref(null);
 const isUploading = ref(false);
-const uploadProgress = ref(0.1);
-const uploadProgressColor = "warning";
-let isLoading = ref(false);
-const isError = ref(false);
-
+const uploadProgress = ref(0);
 const qFileError = ref(null);
+const newAttachments = ref([]);
 
 function cancelFile() {
   return new Promise((resolve) => {
-    // simulating a delay of 2 seconds
     setTimeout(() => {
       resolve((files.value = null));
     }, 1500);
@@ -134,141 +128,100 @@ function cancelFile() {
 }
 
 function onRejected() {
-  qFileError.value = "incorrect file, upload something else";
+  qFileError.value = t("incorrect file, upload something else");
   return new Promise((resolve) => {
-    // simulating a delay of 2 seconds
     setTimeout(() => {
       resolve((qFileError.value = null));
     }, 2500);
   });
 }
 
-let progress = ref(60);
-const compressor = ref(null);
-let compressObj = reactive({
-  size: 0,
-  file: "",
-  name: "",
-});
-
 const compressorFn = () => {
   qFileError.value = null;
   uploadProgress.value = 0;
-  let file = files.value;
-  if (!file) {
-    return;
-  }
-  compressor.value = new Compressor(file, {
+  if (!files.value) return;
+
+  new Compressor(files.value, {
     quality: 0.6,
     maxWidth: 1600,
     mimeType: "image/jpeg",
     success(result) {
-      compressObj.file = result;
-      compressObj.name = result.name;
-      // compressObj.size = Math.round((result.size / 1024) * 100) / 100;
-      // fileToBase64(result, (data) => {
-      //   compressObj.url = data;
-      // });
-
+      const objectURL = URL.createObjectURL(result);
       uploadProgress.value = 0.5;
-
-      // size check
-      let img = new Image();
-      let objectURL = URL.createObjectURL(result);
-      img.onload = function () {
-        console.log(img.width, img.height);
-      };
-      img.src = objectURL;
-
-      console.log(result.size, result.type, result.name, result.lastModified);
-      // console.log(token)
-
-      uploadFile();
-
-      uploadProgress.value = 0;
+      uploadFile(result, objectURL);
     },
     error(err) {
-      console.log(err.message);
+      console.error(err.message);
     },
   });
 };
 
-
-const onUploadProgress = (progressEvent) => {
-  const {loaded, total} = progressEvent;
-  let percent = Math.floor((loaded * 100) / total);
-  if (percent < 100) {
-    console.log(`${loaded} bytes of ${total} bytes. ${percent}%`);
-  }
-};
-
-function uploadFile(file) {
-  let token = props.token;
-  if (props.token == null) token = UserStore.getToken;
-
-  let tenant_id = props.tenant_id;
-  if (props.tenant_id == null) tenant_id = UserStore.getTenant;
+function uploadFile(file, objectURL) {
+  const token = props.token || UserStore.getToken;
+  const tenant_id = props.tenant_id || UserStore.getTenant;
 
   const formData = new FormData();
-  formData.append("file", compressObj.file, compressObj.name); // The third parameter is required for server
+  formData.append("file", file, file.name);
 
-  isLoading.value = true;
+  isUploading.value = true;
   api
-    .post(process.env.VUE_APP_URL + "/files/", formData, {
+    .post(`${process.env.VUE_APP_URL}/files/`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
-        Authorization: "Bearer " + token,
+        Authorization: `Bearer ${token}`,
         tenant: tenant_id,
       },
       onUploadProgress,
     })
     .then((res) => {
       attachments.value.push(res.data);
-      newAttachments.value.push(res.data.uuid)
-
-      console.log(newAttachments.value);
-      console.log(attachments.value);
-      // uploader.value.reset()
-      emit('uploadedPhotos', attachments.value)
-      isLoading.value = false;
+      newAttachments.value.push(res.data.uuid);
+      emit("uploadedPhotos", attachments.value);
+      isUploading.value = false;
       files.value = null;
     })
-    .catch((err) => {
-      if (err.response) {
-        console.log(err.response);
-      } else if (err.request) {
-        console.log(err.request);
-      } else {
-        console.log("General Error " + err);
-      }
-      isLoading.value = false;
-    });
+    .catch(handleUploadError);
 }
 
-async function delete_file(uuid) {
+function handleUploadError(err) {
+  console.error(err.response || err.request || `General Error: ${err}`);
+  isUploading.value = false;
+}
+
+function onUploadProgress(progressEvent) {
+  const percentCompleted = Math.floor((progressEvent.loaded * 100) / progressEvent.total);
+  uploadProgress.value = percentCompleted < 100 ? percentCompleted / 100 : 1;
+}
+
+function deleteFile(uuid) {
   if (newAttachments.value.includes(uuid)) {
-    let token = props.token;
-    if (props.token == null) token = UserStore.getToken;
+    const token = props.token || UserStore.getToken;
+    const tenant_id = props.tenant_id || UserStore.getTenant;
 
-    let tenant_id = props.tenant_id;
-    if (props.tenant_id == null) tenant_id = UserStore.getTenant;
+    isUploading.value = true;
 
-    isLoading.value = true;
-    const {data, error} = await noAuthAPI.delete("/files/" + uuid, {
-      headers: {
-        Authorization: "Bearer " + token,
-        tenant: tenant_id,
-      },
-    })
-    if (error !== null) {
-      console.log('Nie udało się usunąć pliku')
-      return;
-    }
+
+    deleteFileRequest(uuid, token, tenant_id).then(function (response) {
+      console.log("Deleted from Uploader")
+      isLoading.value = false;
+
+    }).catch((err) => {
+      console.log("error")
+    });
+
   }
 
   attachments.value = attachments.value.filter((item) => item.uuid !== uuid);
-  emit('uploadedPhotos', attachments.value)
+  emit("uploadedPhotos", attachments.value);
+  isUploading.value = false;
 }
 
-
 </script>
+
+<style scoped>
+.image-preview {
+  height: 300px;
+  width: 100%;
+  border: 1px solid gray;
+}
+</style>
